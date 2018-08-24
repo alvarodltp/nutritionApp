5098303065f61a1b00fa2b74be54b5c803fd01eb
Alvaro De La Torre <alvarodltp@gmail.com>
2018-08-24 13:42:42 -0400

    calculates and displays macros, fixes display repetition

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -4,6 +4,12 @@ function init() {
   disablePage();
 }
 
+let homePageDiv = document.getElementById("home");
+let homeButton = document.getElementById("home-button");
+// homeButton.addEventListener("click", showHome);
+let foodTrackerButton = document.getElementById("food-tracker");
+foodTrackerButton.addEventListener("click", showCalendarPage);
+
 let firstNameField = document.getElementById("first_name").value;
 let activityLevel = parseFloat(document.getElementById("activity-level").value);
 let calculateButton = document.getElementById("calculate-button");
@@ -15,6 +21,7 @@ calculateButton.addEventListener("click", function(event) {
 });
 let loginButton = document.getElementById("login-button");
 loginButton.addEventListener("click", getUser);
+
 let ectoImage = document.getElementById("ectomorph");
 ectoImage.addEventListener("click", calculateMacros);
 let mesoImage = document.getElementById("mesomorph");
@@ -46,12 +53,14 @@ function displayBmr(event) {
   let gender = document.getElementById("gender").value;
   let showBmr = document.getElementById("show-bmr");
   let h1Element = document.createElement("h1");
+  h1Element.className = "h1-bmr";
+  showBmr.innerHTML = "";
   // debugger;
   if (gender === "male") {
-    h1Element.innerText = `Your BMR(Basal Metabolic Rate) is ${bmrForMen()}. This is the number of calories you would burn if you slept for 24 hours.`;
+    h1Element.innerText = `BMR: ${bmrForMen()}`;
     showBmr.appendChild(h1Element);
   } else if (gender === "female") {
-    h1Element.innerText = `Your BMR(Basal Metabolic Rate) is ${bmrForWomen()}. This is the number of calories you would burn if you slept for 24 hours.`;
+    h1Element.innerText = `BMR: ${bmrForWomen()}`;
     showBmr.appendChild(h1Element);
     // debugger;
   } else {
@@ -62,24 +71,25 @@ function displayBmr(event) {
 // displays calories needed for men or women
 function displayCaloriesNeeded(event) {
   counter = 0;
+  let message = "";
+  let showBmr = document.getElementById("show-bmr");
   let gender = document.getElementById("gender").value;
   let goal = document.getElementById("goal").value;
   let activityLevel = parseFloat(
     document.getElementById("activity-level").value
   );
-  let showBmr = document.getElementById("show-bmr");
+
   let h1Element = document.createElement("h1");
+  h1Element.className = "h1-calories";
   let deficit = 583;
   let calories;
-  let message =
-    "According to your personal information and goal, you need a total of ";
   //this checks for gender and goal in order to calculate calories needed
   if (gender === "male" && goal === "gain") {
     calories = parseFloat(activityLevel * bmrForMen() + deficit).toFixed(2);
     const interval = setInterval(() => {
       if (counter < calories) {
         counter++;
-        h1Element.innerText = counter;
+        h1Element.innerText = `Calories: ${counter}`;
       } else {
         clearInterval(interval);
       }
@@ -90,7 +100,7 @@ function displayCaloriesNeeded(event) {
     const interval = setInterval(() => {
       if (counter < calories) {
         counter++;
-        h1Element.innerText = counter;
+        h1Element.innerText = `Calories: ${counter}`;
       } else {
         clearInterval(interval);
       }
@@ -101,7 +111,7 @@ function displayCaloriesNeeded(event) {
     const interval = setInterval(() => {
       if (counter < calories) {
         counter++;
-        h1Element.innerText = counter;
+        h1Element.innerText = `Calories: ${counter}`;
       } else {
         clearInterval(interval);
       }
@@ -112,7 +122,7 @@ function displayCaloriesNeeded(event) {
     const interval = setInterval(() => {
       if (counter < calories) {
         counter++;
-        h1Element.innerText = counter;
+        h1Element.innerText = `Calories: ${counter}`;
       } else {
         clearInterval(interval);
       }
@@ -123,7 +133,7 @@ function displayCaloriesNeeded(event) {
     const interval = setInterval(() => {
       if (counter < calories) {
         counter++;
-        h1Element.innerText = counter;
+        h1Element.innerText = `Calories: ${counter}`;
       } else {
         clearInterval(interval);
       }
@@ -134,7 +144,7 @@ function displayCaloriesNeeded(event) {
     const interval = setInterval(() => {
       if (counter < calories) {
         counter++;
-        h1Element.innerText = counter;
+        h1Element.innerText = `Calories: ${counter}`;
       } else {
         clearInterval(interval);
       }
@@ -197,7 +207,12 @@ function getUser(event) {
           // debugger;
           enablePage();
           renderUser(user);
-          // debugger;
+          let messageBox = document.getElementById("message");
+          let message = "Congratulations! Your have successfully logged in.";
+          let p = document.createElement("p");
+          p.className = "message-text";
+          p.innerText = message;
+          messageBox.appendChild(p);
           return;
         }
       }
@@ -240,44 +255,85 @@ function createNewUser() {
     .then(response => response.json())
     .then(json => {
       enablePage();
+      let message = "Congratulations! Your account has been created.";
+      let messageBox = document.getElementById("message");
+      let p = document.createElement("p");
+      p.className = "message-text";
+      p.innerText = message;
+      messageBox.appendChild(p);
     });
 }
 
 //function that calculates macronutrients based on selection of bodytype
 
-function addsPCFToMacrosDiv(protein, carbs, fats) {
-  let calories = displayCaloriesNeeded();
-  let macrosDiv = document.getElementById("macros");
-  let h1Protein = document.createElement("h1");
-  let h1Carbs = document.createElement("h1");
-  let h1Fats = document.createElement("h1");
-  h1Protein.innerText = protein;
-  h1Carbs.innerText = carbs;
-  h1Fats.innerText = fats;
-  macrosDiv.appendChild(h1Protein);
-  macrosDiv.appendChild(h1Carbs);
-  macrosDiv.appendChild(h1Fats);
+function calculateMacros(event) {
+  // debugger;
+  let calories = document
+    .getElementsByClassName("h1-calories")[0]
+    .innerText.split(" ")[1];
+  if (calories > 0) {
+    let bodySelected = event.target.parentElement.parentElement.id;
+    if (bodySelected === "ectomorph") {
+      let protein = ((calories * 0.25) / 4).toFixed(2);
+      let carbs = ((calories * 0.55) / 4).toFixed(2);
+      let fats = ((calories * 0.2) / 9).toFixed(2);
+      return addsPCFToMacrosDiv(protein, carbs, fats);
+    } else if (bodySelected === "mesomorph") {
+      let protein = ((calories * 0.3) / 4).toFixed(2);
+      let carbs = ((calories * 0.4) / 4).toFixed(2);
+      let fats = ((calories * 0.3) / 9).toFixed(2);
+      return addsPCFToMacrosDiv(protein, carbs, fats);
+    } else if (bodySelected === "endomorph") {
+      let protein = ((calories * 0.35) / 4).toFixed(2);
+      let carbs = ((calories * 0.25) / 4).toFixed(2);
+      let fats = ((calories * 0.4) / 9).toFixed(2);
+      return addsPCFToMacrosDiv(protein, carbs, fats);
+    } else {
+      return false;
+    }
+  }
 }
 
-function calculateMacros(event) {
-  let calories = displayCaloriesNeeded();
-  let bodySelected = event.target.parentElement.parentElement.id;
-  if (bodySelected === "ectomorph") {
-    let protein = ((calories * 0.25) / 4).toFixed(2);
-    let carbs = ((calories * 0.55) / 4).toFixed(2);
-    let fats = ((calories * 0.2) / 9).toFixed(2);
-    addsPCFToMacrosDiv(protein, carbs, fats);
-  } else if (bodySelected === "mesomorph") {
-    let protein = ((calories * 0.3) / 4).toFixed(2);
-    let carbs = ((calories * 0.4) / 4).toFixed(2);
-    let fats = ((calories * 0.3) / 9).toFixed(2);
-    addsPCFToMacrosDiv(protein, carbs, fats);
-  } else if (bodySelected === "endomorph") {
-    let protein = ((calories * 0.35) / 4).toFixed(2);
-    let carbs = ((calories * 0.25) / 4).toFixed(2);
-    let fats = ((calories * 0.4) / 9).toFixed(2);
-    addsPCFToMacrosDiv(protein, carbs, fats);
-  }
+function addsPCFToMacrosDiv(protein, carbs, fats) {
+  //get each image, the title, and clear the page
+  let bodyTitle = document.getElementById("select-body-title");
+  bodyTitle.innerHTML = "Here are your personalized macronutrients.";
+  let ectoImage = document.getElementById("ecto-image");
+  ectoImage.innerHTML = "";
+  let mesoImage = document.getElementById("meso-image");
+  mesoImage.innerHTML = "";
+  let endoImage = document.getElementById("endo-image");
+  endoImage.innerHTML = "";
+  let ectoTitle = document.getElementById("ecto-title");
+  ectoTitle.innerHTML = "";
+  let mesoTitle = document.getElementById("meso-title");
+  mesoTitle.innerHTML = "";
+  let endoTitle = document.getElementById("endo-title");
+  endoTitle.innerHTML = "";
+  let calories = document
+    .getElementsByClassName("h1-calories")[0]
+    .innerText.split(" ")[1];
+  let h1Protein = document.createElement("h1");
+  h1Protein.className = "protein-text";
+  let h1Carbs = document.createElement("h1");
+  h1Carbs.className = "carbs-text";
+  let h1Fats = document.createElement("h1");
+  h1Fats.className = "fats-text";
+  h1Protein.innerText = `${protein}g`;
+  h1Carbs.innerText = `${carbs}g`;
+  h1Fats.innerText = `${fats}g`;
+  let h2Protein = document.createElement("h2");
+  let h2Carbs = document.createElement("h2");
+  let h2Fats = document.createElement("h2");
+  h2Protein.innerText = "Protein";
+  h2Carbs.innerText = "Carbs";
+  h2Fats.innerText = "Fats";
+  ectoImage.appendChild(h1Protein);
+  mesoImage.appendChild(h1Carbs);
+  endoImage.appendChild(h1Fats);
+  ectoTitle.appendChild(h2Protein);
+  mesoTitle.appendChild(h2Carbs);
+  endoTitle.appendChild(h2Fats);
 }
 
 function updateUser(event) {
@@ -318,4 +374,8 @@ function updateUser(event) {
   });
   // .then(response => response.json())
   // .then(json => {});
+}
+
+function showCalendarPage(event) {
+  homePageDiv.style.display = "none";
 }
